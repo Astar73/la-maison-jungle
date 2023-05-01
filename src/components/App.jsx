ff92e2d89e03ee2c4fde07386d2f402cc3be11a2
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2023-05-01 12:27:43 +0200

    Creating Cart component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Banner } from './Banner'
+import { Cart } from './Cart'
 
 function App() {
     return (
         <div className="App">
             <Banner />
+            <Cart />
         </div>
     )
 }
