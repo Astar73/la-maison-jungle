da1d8c693c0ad1cc7b7fe084d00bb1e576c11548
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2023-05-02 12:07:44 +0200

    Update ShoppingList.jsx

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -21,6 +21,7 @@ export const ShoppingList = () => {
             <ul className="lmj-plant-list">
                 {plantList.map((plant) => (
                     <li key={plant.id} className="lmj-plant-item">
+                        {plant.isBestSale && <span>🔥</span>}
                         {plant.name}
                         {plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
                         <CareScale careType="water" scaleValue={plant.water} />
