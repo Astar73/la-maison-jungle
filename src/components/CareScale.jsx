d05bda0407631800c4e2e0bd6ab85a1e552cfe1d
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2023-05-02 12:07:37 +0200

    Update CareScale.jsx

--- src/components/CareScale.jsx
+++ src/components/CareScale.jsx
@@ -1,11 +1,13 @@
 export const CareScale = ({ scaleValue, careType }) => {
     const range = [1, 2, 3]
-    
     const scaleType = careType === 'light' ? '☀️' : '💧'
 
     return (
         <div>
-            {range.map((rangeElem) => scaleValue >= rangeElem ? <span key={rangeElem.toString()}>{scaleType}</span> : null
+            {range.map((rangeElem) =>
+                scaleValue >= rangeElem ? (
+                    <span key={rangeElem.toString()}>{scaleType}</span>
+                ) : null
             )}
         </div>
     )
