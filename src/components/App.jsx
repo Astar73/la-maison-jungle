77fc5666dcb60f5357fac4535177bb2b9d44bce9
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2023-05-02 12:09:56 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import { Banner } from './Banner'
+import logo from '../assets/logo.png'
 import { Cart } from './Cart'
 import { ShoppingList } from './ShoppingList'
 
 export const App = () => {
     return (
         <div className="App">
-            <Banner />
-            <Cart />            
+            <Banner>
+                <img src={logo} alt="La maison jungle" className="lmj-logo" />
+                <h1 className="lmj-title">La maison jungle</h1>
+            </Banner>
+            <Cart />
             <ShoppingList />
         </div>
     )
