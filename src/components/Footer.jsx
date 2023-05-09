eac02dda9022888e8501c06fc90692e33f5c0f89
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2023-05-09 16:47:18 +0200

    create event

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,29 @@ import '../styles/Footer.css'
 export const Footer = () => {
     const [inputValue, setInputValue] = useState('')
 
+    const handleInput = (event) => {
+        setInputValue(event.target.value)
+    }
+
+    const handleBlur = () => {
+        if (!inputValue.includes('@')) {
+            alert(`Attention, il n'y a pas d'@, ceci n'est pas une adresse valide.`)
+        }
+    }
+
     return (
         <footer className="lmj-footer">
             <div className="lmj-footer-element">
-                Pour les passionné·e·s de plantes 🌿🌱🌵
+                Pour les passionné.e.s de plantes 🌿🌱🌵
             </div>
             <div className="lmj-footer-element">Laissez-nous votre mail :</div>
+            <input
+                placeholder="Entrez votre mail"
+                value={inputValue}
+                onChange={handleInput}
+                onBlur={handleBlur}
+            />
+            <button onClick={() => alert(inputValue)}>Envoyer</button>
         </footer>
     )
 }
