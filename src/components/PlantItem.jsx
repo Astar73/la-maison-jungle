ccb8b04ef610bada082f732067199afa6ccbb012
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2023-05-09 14:31:46 +0200

    add test event

--- src/components/PlantItem.jsx
+++ src/components/PlantItem.jsx
@@ -2,8 +2,11 @@ import { CareScale } from './CareScale'
 import '../styles/PlantItem.css'
 
 export const PlantItem = ({ name, cover, id, light, water }) => {
+    const handleClick = (plantName) => {
+        alert(`Vous avez acheter 1 ${plantName}`)
+    }
     return (
-        <li key={id} className="lmj-plant-item">
+        <li className="lmj-plant-item" onClick={() => handleClick(name)}>
             <img className="lmj-plant-item-cover" src={cover} alt={`${name} cover`} />
             {name}
             <div>
