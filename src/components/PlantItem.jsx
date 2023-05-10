b749eca4614fc8ab5c98d24c5b9cfbf397e0eb05
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2023-05-10 15:04:22 +0200

    Update PlantItem.jsx

--- src/components/PlantItem.jsx
+++ src/components/PlantItem.jsx
@@ -1,12 +1,15 @@
 import { CareScale } from './CareScale'
 import '../styles/PlantItem.css'
 
-export const PlantItem = ({ name, cover, id, light, water }) => {
+export const PlantItem = ({ name, cover, light, water, price }) => {
     const handleClick = (plantName) => {
-        alert(`Vous avez acheter 1 ${plantName}`)
+        alert(
+            `Voulez-vous avez acheter un(e) ${plantName.toUpperCase()} pour ${price}€ ?`
+        )
     }
     return (
-        <li key={id} className="lmj-plant-item" onClick={() => handleClick(name)}>
+        <li className="lmj-plant-item" onClick={() => handleClick(name)}>
+            <span className="lmj-plant-item-price">{price}€</span>
             <img className="lmj-plant-item-cover" src={cover} alt={`${name} cover`} />
             {name}
             <div>
